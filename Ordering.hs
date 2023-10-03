7ff097ef6e75340febe241e3b8c32dd2197a6d18
MariaPaz04 <mpazmarcato@gmail.com>
2023-10-03 16:56:02 -0300

    adjusting because of error

--- Ordering.hs
+++ Ordering.hs
@@ -1,13 +1,6 @@
 module Ordering where
 
 import Prelude hiding ((<), (>), compare)
-import DataTypes
-
-compare :: Nat -> Nat -> Ordering
-compare O O = EQ
-compare _ O = GT
-compare O _ = LT
-compare (S n) (S m) = compare n m
 
 (<) :: Nat -> Nat -> Bool
 n < m = compare n m == LT
@@ -20,3 +13,9 @@ n >= m = n > m || n == m
 
 (<=) :: Nat -> Nat -> Bool
 n <= m = n < m || n == m
+
+compare :: Nat -> Nat -> Ordering
+compare O O = EQ
+compare _ O = GT
+compare O _ = LT
+compare (S n) (S m) = compare n m
