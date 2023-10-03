5abba417d2197faa5a85debeae53cb4a436a4be6
MariaPaz04 <mpazmarcato@gmail.com>
2023-10-03 20:53:24 -0300

    Fixed bug

--- Ordering.hs
+++ Ordering.hs
@@ -3,20 +3,20 @@ module Ordering where
 import Prelude hiding ((<), (>), compare)
 import Nat
 
+(<) :: Nat -> Nat -> Bool
+n < m = compare n m == LT
+
+(>=) :: Nat -> Nat -> Bool
+n >= m = n > m || n == m 
+
+(>) :: Nat -> Nat -> Bool
+n > m = compare n m == GT
+
+(<=) :: Nat -> Nat -> Bool
+n <= m = n < m || n == m
+
 compare :: Nat -> Nat -> Ordering
 compare O O = EQ
 compare _ O = GT
 compare O _ = LT
 compare (S n) (S m) = compare n m
-
-(<) :: Nat -> Nat -> Bool
-n < m = compare n m == LT
-
-(>) :: Nat -> Nat -> Bool
-n > m = compare n m == GT
-
-(>=) :: Nat -> Nat -> Bool
-n >= m = n > m || n == m 
-
-(<=) :: Nat -> Nat -> Bool
-n <= m = n < m || n == m
